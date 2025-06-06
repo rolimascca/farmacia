efe8cd63b1f6427ac3c21fd10543a86b11ce257b
unknown <rolando@email.com>
2025-06-06 12:36:35 -0500

    borar el static_root

--- practica_soa/settings.py
+++ practica_soa/settings.py
@@ -126,9 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-if not DEBUG:
-    STATIC_ROOT = os.phath.join(BASE_DIR, 'staticfiles')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
     
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
