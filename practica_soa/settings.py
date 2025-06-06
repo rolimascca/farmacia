40018123e89303c995c8405d4e8b1d8c89e2b8f9
unknown <rolando@email.com>
2025-06-06 12:22:12 -0500

    cambiar el erros de static

--- practica_soa/settings.py
+++ practica_soa/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 if not DEBUG:
-    STATI_ROOT = os.phath.join(BASE_DIR, 'staticfiles')
+    STATIC_ROOT = os.phath.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
     
 # Default primary key field type
